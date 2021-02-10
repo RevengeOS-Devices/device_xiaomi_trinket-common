cfde773372915d4dc10138a4bba4a7da0df75a7f
Introdructor <yashkachave7@gmail.com>
2021-02-10 16:57:08 +0530

    trinket-common:User Builds now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -157,7 +157,6 @@ TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
-SELINUX_IGNORE_NEVERALLOWS := true
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Shims
